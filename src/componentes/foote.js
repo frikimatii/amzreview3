bb20de5cf1642952fee79683f997b1bcff80ca4f
Frikimatii7 <matibiencomodo@gmail.com>
2024-02-10 16:12:05 -0300

    agregado de pestania herramientas y mas productos , y correccion de errores y del SEO 2

--- src/componentes/foote.js
+++ src/componentes/foote.js
@@ -7,19 +7,34 @@ function Footer() {
     <footer className="mt-5 bg-dark text-light p-4 footer">
       <Container fluid className="text-center">
         <div className="mb-3">
-          <a href="https://www.facebook.com/profile.php?id=100094886682039" className="text-light me-3">
+          <a
+            href="https://www.facebook.com/profile.php?id=100094886682039"
+            target="https://www.facebook.com/profile.php?id=100094886682039"
+            className="text-light me-3"
+          >
             <FaFacebook size={30} />
           </a>
-          <a href="https://twitter.com/reviewAMZReview" className="text-light me-3">
+          <a
+            href="https://twitter.com/reviewAMZReview"
+            target="https://twitter.com/reviewAMZReview"
+            className="text-light me-3"
+          >
             <FaTwitter size={30} />
           </a>
-          <a href="https://www.instagram.com/amzreview3/" className="text-light">
+          <a
+            href="https://www.instagram.com/amzreview3/"
+            target="https://www.instagram.com/amzreview3/"
+            className="text-light"
+          >
             <FaInstagram size={30} />
           </a>
         </div>
         <p>&copy; 2024 AMZ Review. Todos los derechos reservados.</p>
         <p>@frikimatii | matibiencomodo@gmail.com</p>
-        <p><a href="/privacy">Política de Privacidad</a> | <a href="/terms">Términos de Servicio</a></p>
+        <p>
+          <a href="/privacy">Política de Privacidad</a> |{" "}
+          <a href="/terms">Términos de Servicio</a>
+        </p>
       </Container>
     </footer>
   );
